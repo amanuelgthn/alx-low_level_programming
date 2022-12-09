f2f3ed498bc9996a85e5a76163c964a7a6e35ebe
amanuelgthn <amanuelgthn@gmail.com>
2022-12-08 19:50:53 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- *
  *main - Entry point
  *
  *Description - Print hell world
  *
  *Return: Always 0 (Success)
- *
 **/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
