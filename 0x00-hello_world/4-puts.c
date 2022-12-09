3c7ecd0af86e192059f36cbb7cc24c8ff00b0b99
amanuelgthn <amanuelgthn@gmail.com>
2022-12-08 18:20:22 -0800

    -puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,8 @@
  *
  *    * Return: Always 0 (Success)
  *
- *     */
+ **/
 int main (void)
-{
-	puts ("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);}
 
