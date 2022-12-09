ac32a0fb29dfcc01d180abe1d22c1f00e8359308
amanuelgthn <amanuelgthn@gmail.com>
2022-12-08 20:03:44 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Descriptioni - Using printf
+ *Return: Always 0 (Success)
+ **/
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,");
+	return (0); }
