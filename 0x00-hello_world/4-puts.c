5b55910de4c45c99cfa685d72f3d20ae61b7bc0c
amanuelgthn <amanuelgthn@gmail.com>
2022-12-08 19:48:35 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  *
- *  * main - Entry point
+ *main - Entry point
  *
- *   *Description - Print hell world
+ *Description - Print hell world
  *
- *    * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  *
 **/
 int main(void)
