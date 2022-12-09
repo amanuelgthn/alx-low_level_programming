84f1606e76eb3075352c41e0ada1fa4776288dbd
amanuelgthn <amanuelgthn@gmail.com>
2022-12-08 19:00:49 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 /**
- * Description-this is a hello world function
- * */
+ *
+ *  * main - Entry point
+ *
+ *   *Description - Print hell world
+ *
+ *    * Return: Always 0 (Success)
+ *
+**/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 	return (0)	; }
