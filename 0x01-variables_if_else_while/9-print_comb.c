919b814bb26759690117dcdf9cf414ce7c3b6fd4
amanuelgthn <amanuelgthn@gmail.com>
2022-12-10 05:51:25 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,11 @@ int main(void)
         for (i = 0; i < 10; i++)
 	{
 		putchar((i) + '0');
+		if (i < 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
         putchar('\n');
         return (0);
