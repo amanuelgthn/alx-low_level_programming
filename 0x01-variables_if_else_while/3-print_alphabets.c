4c2161182aa776efc2d7095793a62535a83568d4
amanuelgthn <amanuelgthn@gmail.com>
2022-12-09 19:03:59 -0800

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -25,12 +25,8 @@ int main(void)
         char i;
 
         for (i = 'a'; i <= 'z'; i++)
-	{	putchar (i);
-	}
-	for (i = 'A'; i <= 'Z'; i++)
-	{
-		putchar (i);
-	}
-	putchar ('\n');
-	return (0);
+                putchar (i);
+        putchar ('\n');
+return (0);
 }
+
