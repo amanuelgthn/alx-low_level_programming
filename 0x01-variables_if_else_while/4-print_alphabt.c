ced38c1614b25955d28ca9fba6e012467b6aaae4
amanuelgthn <amanuelgthn@gmail.com>
2022-12-09 19:49:53 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,12 @@
 int main(void)
 {
 	char i;
-
-        for (i = 'a'; i <= 'z'; i++)
+	
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		if(i !='q' && i != 'e')
+		if (i != 'q' && i != 'e')
 			putchar (i);
 	}
 	putchar ('\n');
 return (0);
 }
-
