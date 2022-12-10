e50df176d1251d50ace7f98b647d6de43ce95b95
amanuelgthn <amanuelgthn@gmail.com>
2022-12-10 01:11:52 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Description - if statements
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i++)
+                putchar((i) + '0')
+		putchar(',');
+        putchar('\n');
+        return (0);
+} 
