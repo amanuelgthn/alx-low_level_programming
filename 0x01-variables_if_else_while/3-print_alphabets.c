fefbe9769d755285ceb224b5c74430928b8f766d
amanuelgthn <amanuelgthn@gmail.com>
2022-12-09 18:53:00 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Description - if statements
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char i;
+
+        for (i = 'a'; i <= 'z'; i++)
+                putchar (i);
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Description - if statements
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char i;
+
+        for (i = 'a'; i <= 'z'; i++)
+                putchar (i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar (i);
+	putchar ('\n');
+return (0);
+}
