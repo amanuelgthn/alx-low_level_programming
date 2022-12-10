cfe57797e73f60d28e792f988914e6c40b650706
amanuelgthn <amanuelgthn@gmail.com>
2022-12-09 19:00:47 -0800

    {         char i;
            for (i = 'a'; i <= 'z'; i++)
                    putchar (i);
            putchar ('\n');
    return (0);
    }
    '

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -25,9 +25,12 @@ int main(void)
         char i;
 
         for (i = 'a'; i <= 'z'; i++)
-                putchar (i);
+	{	putchar (i);
+	}
 	for (i = 'A'; i <= 'Z'; i++)
+	{
 		putchar (i);
+	}
 	putchar ('\n');
 	return (0);
 }
