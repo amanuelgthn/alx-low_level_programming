bd00a7c2a10154cfc6f36599a252387dfc55a26d
amanuelgthn <amanuelgthn@gmail.com>
2022-12-09 19:44:54 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Description - if statements
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+        for (i = 'a'; i <= 'z'; i++)
+	{
+		if(i !='q' && i != 'e')
+			putchar (i);
+	}
+	putchar ('\n');
+return (0);
+}
+
