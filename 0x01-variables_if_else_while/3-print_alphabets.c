8ec73c74f02f828c7c70982d5e57d8b3168d2774
amanuelgthn <amanuelgthn@gmail.com>
2022-12-09 19:15:25 -0800

    k

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,12 @@
  */
 int main(void)
 {
-        char i;
-
-        for (i = 'a'; i <= 'z'; i++)
-                putchar (i);
-        putchar ('\n');
+       	char i;
+       
+	for (i = 'a'; i <= 'z'; i++)
+		putchar (i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar (i);
+	putchar ('\n');
 return (0);
 }
