786a3cd90b42794183ba6712545a099ac663f3ee
amanuelgthn <amanuelgthn@gmail.com>
2022-12-11 06:11:48 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,8 +10,9 @@ int main(void)
 {
         int i;
         int j;
-
-        for (i = 0; i < 8; i++)
+	int k;
+	
+	for (i = 0; i < 8; i++)
         {
                 for (j = i + 1; j < 9; j++)
 		{
