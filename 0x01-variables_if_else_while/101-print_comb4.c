dac8ed3e725d53c9b8220cb3745c55cc42d1b2c5
amanuelgthn <amanuelgthn@gmail.com>
2022-12-11 06:10:28 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Description - if statements
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+        int j;
+
+        for (i = 0; i < 8; i++)
+        {
+                for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				if (j > i && k > j)
+				{
+					putchar((i) + '0');
+				}
+				{
+					putchar((j) + '0');
+				}
+				{
+					putchar((k) + '0');
+					if (j == 8 && i == 7 && k==9)
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
