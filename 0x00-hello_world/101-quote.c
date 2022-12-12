e76099a21378dcc3db5508d145e013dfda7782c5
amanuelgthn <amanuelgthn@gmail.com>
2022-12-12 04:40:20 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *Description - Write file 
+ *Description - Write file
  *Return: Always 0 (Success)
  **/
-int main (void)
+int main(void)
 {
-	char c[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int count;
-	for (count = 0; count < 57; count++)
+
+	for (count = 0; count < 58; count++)
 	{
 		if (c[count] == '\0')
 		{
