73346757a163f56440f60f47d9e65a4624a9629c
amanuelgthn <amanuelgthn@gmail.com>
2022-12-11 21:13:19 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description - Write file 
+ *Return: Always 0 (Success)
+ **/
+int main (void)
+{
+	char="and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	putchar(char);
+	putchar('\n');
+	return (1);
+}
