b97cf5ad28779b74e7fd9b0c384daaf5f99e11c2
amanuelgthn <amanuelgthn@gmail.com>
2022-12-13 03:27:49 -0800

    {   printf(_);   printf(putchar);   return 0;   }

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Description - if statements
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  printf("_");
+  printf("putchar");
+  return (0);
+  }
