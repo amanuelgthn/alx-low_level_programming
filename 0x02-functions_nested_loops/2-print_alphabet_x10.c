d6dc3fd0259fa1d66fd3fc59cce4e362e1baaaff
amanuelgthn <amanuelgthn@gmail.com>
2022-12-14 05:30:49 -0800

    /**  *print_alphabet - prints alphabet  *print_alphabet_x10 - prints alphabet **/

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 /**
  *print_alphabet - prints alphabet
- *print_alphabet_x10 - prints alphabet
 **/
 void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-        {
+	{
 		putchar (i);
 	}
 	putchar ('\n');
 }
+/**
+ *print_alphabet_x10 - prints alphabet
+**/
+
 void print_alphabet_x10(void)
 {
 	int i;
