6e18c2b7f44a80cf364dacccc8840eef23dd6f65
amanuelgthn <amanuelgthn@gmail.com>
2022-12-14 14:50:04 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		return(1);
 		putchar('+');
-		return (1);
 	}
 	else if (n == 0)
 	{
+		return(0);
 		putchar(0 + '0');
-		return (1);
 	}
 	else
 	{
+		return(-1);
 		putchar('-');
-		return (-1);
 	}
 }
