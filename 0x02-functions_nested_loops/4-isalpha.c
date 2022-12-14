8f6d7b8b281899d3228544474908e809b1dcbd8b
amanuelgthn <amanuelgthn@gmail.com>
2022-12-14 14:25:45 -0800

    else if (c >= 'A' && c >= 'Z')         {                 return (1);         }

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,11 +6,11 @@
 **/
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
-	else if (c >= 'A' && c >= 'Z')
+	else if (c >= 'a' && c >= 'z')
 	{
 		return (1);
 	}
