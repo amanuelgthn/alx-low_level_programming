7e0bcfc67106bef801d5ecefc35b25244d806f32
amanuelgthn <amanuelgthn@gmail.com>
2022-12-14 04:42:23 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  *main - Entry point
- **Description - if statements
+ *Description - function to print alphabet
  *Return: Always 0 (Success)
  **/
 void print_alphabet(void)
 {
 	char i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar (i);
