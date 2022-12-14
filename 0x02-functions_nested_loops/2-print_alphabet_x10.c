f1e23af2ebf768619344d1f8c53a1a15c54de250
amanuelgthn <amanuelgthn@gmail.com>
2022-12-14 05:21:56 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,17 @@
 /**
  *print_alphabet_x10 - prints alphabet
 **/
+void print_alphabet(void)
+{
+        char i;
+
+        for (i = 'a'; i <= 'z'; i++)
+        {
+                putchar (i);
+        }
+        putchar ('\n');
+}
+
 void print_alphabet_x10(void)
 {
 	int i;
