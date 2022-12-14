99acaebe60b8ebda837b9c5afb8c15c5c0c583e4
amanuelgthn <amanuelgthn@gmail.com>
2022-12-13 18:26:13 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+nclude <time.h>                                                                                
+
+/**                                                                                              
+ *main - Entry point                                                                             
+ **Description - if statements                                                                    *Return: Always 0 (Success)                                                                     
+ **/
+char print_alphabet();
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+return (0);
+}
