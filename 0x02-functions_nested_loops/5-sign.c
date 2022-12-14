18e7d71139e10daf1b462823c762a0034b63c4db
amanuelgthn <amanuelgthn@gmail.com>
2022-12-14 14:42:51 -0800

    int print_sign(int n);

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *print_sign- a function that checks for lowercase character.
+ *@n: number
+ *Return: Description of the returned value
+**/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n = 0)
+	{
+		putchar(0 + '0');
+		return (1);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
