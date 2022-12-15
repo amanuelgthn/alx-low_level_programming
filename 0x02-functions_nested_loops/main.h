f2c4b4dc695f83a76b20ad8de8c4949070d875b5
amanuelgthn <amanuelgthn@gmail.com>
2022-12-14 17:59:28 -0800

    hh

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
-#include <string.h>
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _putchar(char c);
 int _isalpha(int c);
 int print_sign(int n);
