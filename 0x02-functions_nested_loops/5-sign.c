195d5d9baec14e4302836ac22facc52eb0c32a8c
amanuelgthn <amanuelgthn@gmail.com>
2022-12-14 16:11:31 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,16 +9,13 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return(1);
-		putchar(+);
 	}
 	else if (n == 0)
 	{
 		return(0);
-		putchar(0 + '0');
 	}
 	else
 	{
 		return(-1);
-		putchar(-);
 	}
 }
