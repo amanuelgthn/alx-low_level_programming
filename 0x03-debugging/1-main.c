838e2f5b9a9205d709edac13268935898ddfa344
amanuelgthn <amanuelgthn@gmail.com>
2022-12-15 15:55:54 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,10 +12,11 @@ int main(void)
 
 	i = 0;
 
-/*while (i < 10)**/
-/*	{
-/*		putchar(i);
-/*	}
+/**
+*Description - while (i < 10)
+*	{
+*		putchar(i);
+*	}
 */
 	printf("Infinite loop avoided! \\o/\n");
 
