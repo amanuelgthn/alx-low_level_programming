57e88e5f5d53d49a43d43e6d09d50c892aa30971
amanuelgthn <amanuelgthn@gmail.com>
2022-12-18 02:54:42 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *print_sign- a function that checks for lowercase character.
+ *_abs- a function that checks for lowercase character.
  *@n: number
  *Return: Description of the returned value
 **/
 int _abs(int n)
 {
-	int n;
-
 	return sqrt(n * n);
 }
 
