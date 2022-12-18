0c8efb97404078a9afd3979fa117cb5ede442752
amanuelgthn <amanuelgthn@gmail.com>
2022-12-18 03:52:12 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,20 +8,13 @@
 **/
 int _abs(int n)
 {
-	if (n > 0)
+	if (n >= 0)
         {
-                _putchar('+');
-                return (1);
-        }
-        else if (n == 0)
-        {
-                _putchar(0 + '0');
-                return (0);
-        }
+                _putchar(n + '0');
+	}
         else
         {
-                _putchar('-');
-                return (-1);
+                _putchar(-n + '0');
         }
 }
 
