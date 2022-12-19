74e1cf31781de9aef4b4cc50382ba9b7b44601c1
amanuelgthn <amanuelgthn@gmail.com>
2022-12-19 14:50:04 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *reset to 98 - return 98
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+   *n= 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+"0x05-pointers\_arrays\_strings\n"
