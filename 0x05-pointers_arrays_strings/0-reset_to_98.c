1df7923cadd3eeb4677f76c1cc678ea25730a91b
amanuelgthn <amanuelgthn@gmail.com>
2022-12-19 16:01:37 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 
 /**
-* main - check the code
-*  reset_to_98 - Short description, single line
-*reset to 98 - return 98
-*Return: Always 0.
-*/
+**reset_to_98 - Short description, single line
+**@n:input nuber
+**Return: Always 0.
+**/
 void reset_to_98(int *n)
 {
-	*n= 98;
+	*n = 98;
 }
 
