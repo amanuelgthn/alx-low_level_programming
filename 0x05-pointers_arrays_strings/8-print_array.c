d68e8db6535bae711352c92e06360ea1b6e43b95
amanuelgthn <amanuelgthn@gmail.com>
2022-12-20 07:09:05 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *print_array- Short description, single line
+ *@a:input nuber
+ *@n:input nuber
+ *Return: Always 0.
+ **/
+void print_array(int *a, int n)
+{
+	int n;
+	int i;
+
+	n = sizeof(a);
+	for (i = 0; i < len; i++)
+	{
+		printf("%d", a[i]);
+		printf(", ")
+	}
+	printf("\n");
+}
