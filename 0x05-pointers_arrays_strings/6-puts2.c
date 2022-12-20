b379871eec2aeebc7ebc82e803a4e1e39a78cf67
amanuelgthn <amanuelgthn@gmail.com>
2022-12-20 06:06:47 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *puts2- Short description, single line
+ *@str:input nuber
+ *Return: Always 0.
+ **/
+void puts2(char *str)
+{
+	int len;
+	int i;
+
+	len = strlen(str);
+	for (i = 0; i < len / 2; i = i + 2)
+	{
+		printf("%c", str[i]);
+	}
+}
