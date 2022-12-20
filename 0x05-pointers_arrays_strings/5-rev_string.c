1dd6e234e975794911eb6f802d751dfccfcbda84
amanuelgthn <amanuelgthn@gmail.com>
2022-12-19 18:51:38 -0800

    5-rev_string.c git push

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - Short description, single line
+ *@s:input nuber
+ *Return: Always 0.
+ **/
+void rev_string(char *s)
+{
+	int len;
+	int i;
+	int j;
+	char *k;
+
+	len = strlen(s);
+	k = s;
+	for (i = len - 1, j = 0 ; i >= 0 && j < len; j++, --i)
+	{
+		s[j] = k[i];
+	}
+}
