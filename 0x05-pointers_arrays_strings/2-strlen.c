2a4ea0e582189f3508a0f026806973c2b9313d6a
amanuelgthn <amanuelgthn@gmail.com>
2022-12-19 17:20:25 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strlen- Short description, single line
+ *@s:input nuber
+ *Return: Always 0.
+ **/
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
