04312c13f4cae256617b3736913496e6c0b5c6f2
amanuelgthn <amanuelgthn@gmail.com>
2022-12-19 17:32:43 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ *_puts - Short description, single line
+ *@str:input nuber
+ *Return: Always 0.
+ **/
+void _puts(char *str)
+{
+	printf("%s\n", *str);
+}
