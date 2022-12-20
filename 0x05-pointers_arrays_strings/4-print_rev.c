62c5287a3159e98d641441c0fda46d10ca38c223
amanuelgthn <amanuelgthn@gmail.com>
2022-12-19 18:25:02 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *print_rev- Short description, single line
+ *@s:input nuber
+ *Return: Always 0.
+ **/
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = strlen(s);
+	for (int i = len; i >= 0; --i)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
