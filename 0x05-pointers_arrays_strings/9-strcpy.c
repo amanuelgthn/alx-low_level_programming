07faae60b33c66a4e10d715b5c90fc9d804c35f4
amanuelgthn <amanuelgthn@gmail.com>
2022-12-21 21:16:04 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,13 +13,9 @@ char *_strcpy(char *dest, char *src)
 	int n;
 
 	n = strlen(src);
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
-		for ( ; i < n; i++)
-		{
-			dest[i] = '\0';
-		}
 	}
 	return (dest);
 }
