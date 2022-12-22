458422337e9c54d83ed44d58c06b0e11d27d2f81
amanuelgthn <amanuelgthn@gmail.com>
2022-12-21 21:50:48 -0800

    0-strcat.c.:

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strcpy - Short description, single line
+ *@dest:input nuber
+ *@src:string to be copied
+ *Return: Always 0.
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int n;
+	n = strlen(src);
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
