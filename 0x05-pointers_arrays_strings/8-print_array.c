b442d747973c0741ccbda9e52a6c4ad96410ca66
amanuelgthn <amanuelgthn@gmail.com>
2022-12-21 20:24:55 -0800

    8-print_array.c:

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,13 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	n = sizeof(a);
-	for (i = 0; i < n && a[i] != '\0'; i++)
+	for (i = 0; i < n ; i++)
 	{
 		printf("%d", a[i]);
+		if( i == n - 1)
+		{
+			break;
+		}
 		printf(", ");
 	}
 	printf("\n");
