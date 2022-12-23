f8c91169e33d1143bd925cf3ec8cd526503e530c
amanuelgthn <amanuelgthn@gmail.com>
2022-12-22 19:02:10 -0800

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strncpy- Short description, single line
+ *@dest:input nuber
+ *@src:string to be copied
+ *@n:length
+ *Return: Always 0.
+ **/
+int _strcmp(char *s1, char *s2)
+
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * reverse_array - reversing array
+ * @a:arraay
+ * @n:size of array
+ * Return - o
+ * **/
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	i = n - 1;
+	while (i >= 0)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i--;
+	}
+	printf("\n");
+
+}
+
+
