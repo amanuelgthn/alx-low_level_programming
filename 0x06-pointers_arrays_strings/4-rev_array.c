5b2090d0e5c9087acbf600a9ccf80aa2e63b2571
amanuelgthn <amanuelgthn@gmail.com>
2022-12-23 17:04:56 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,18 +14,13 @@ void reverse_array(int *a, int n)
 	i = n - 1;
 	while (i >= 0)
 	{
-		if (i != 0)
+		if (i != n - 1)
 		{
 			printf(", ");
 		}
 		printf("%d", a[i]);
 		i--;
-		if (i != 0)
-		{
-			printf(", ");
-		}
 	}
 	printf("\n");
-}
 
 
