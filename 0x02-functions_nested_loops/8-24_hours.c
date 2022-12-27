6f1add4d8631ed7f0c3bfa382b84aa6e84ad3fb3
amanuelgthn <amanuelgthn@gmail.com>
2022-12-26 22:22:14 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *jack_bauer - print time
+ *Return - 0 sucess
+ **/
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0 ; i < 24; i++)
+	{
+		for (j = 0 ; j < 60 ; j++)
+		{
+			_putchar(i + '0');
+			_putchar(':');
+			_putchar(j + '0');
+		}
+	}
+}
