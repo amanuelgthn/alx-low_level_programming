febca7814446452a1667a91f11744beb6b60696c
amanuelgthn <amanuelgthn@gmail.com>
2022-12-27 17:10:10 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *times_table - Times table 
+ *times_table - Times table
  *return - 0 sucess
  **/
 void times_table(void)
 {
 	int i, j, k;
-	
-	for (i = 0, i < 10, i++;)
+
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0, j < 10, j++;)
+		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
 			printf("%d", k);
@@ -20,4 +20,3 @@ void times_table(void)
 		printf("\n");
 	}
 }
-			
