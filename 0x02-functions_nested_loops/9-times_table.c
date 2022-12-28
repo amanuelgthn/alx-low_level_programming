f7439cfa612c32d9fb568b7eb2b0dd69edf0f8a7
amanuelgthn <amanuelgthn@gmail.com>
2022-12-27 19:06:36 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			if (k < 10)
+			if (k < 10 && i > 0)
 			{
 				printf(" ");
 			}
@@ -23,7 +23,7 @@ void times_table(void)
 			{
 				break;
 			}
-			printf(",");
+			printf(", ");
 		}
 		printf("\n");
 	}
