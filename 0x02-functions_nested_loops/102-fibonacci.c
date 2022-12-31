cd90d7cc404cc500356948f418167f86614cef9e
amanuelgthn <amanuelgthn@gmail.com>
2022-12-30 21:22:01 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ *main - print first 50 fibonacci numbers
+ *Return: 0 success
+ **/
+int main(void)
+{
+	int a,i,j,k;
+	
+	j == 1;
+	k == 2;
+	printf("%d",j);
+	printf(", ");
+	printf("%d",k);
+	printf(", ");
+
+	while(i == 0 ;i < 50)
+	{
+		a = k;
+		k = k + j;
+		j = a;
+		printf("%d", k);
+		printf(", ");
+		i = i + 1;
+	}
+	return (0);
+}
+
+
