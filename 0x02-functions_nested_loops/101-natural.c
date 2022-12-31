a3fb88d4ded1b99aadb71fa0a2513b02acd2981d
amanuelgthn <amanuelgthn@gmail.com>
2022-12-30 18:29:15 -0800

    101-natural.c:

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - print sum of natural numbers
+ *Return: 0 sucess
+**/
+int main(void)
+{
+	int i;
+	int k;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			k = k + i;
+		}
+	}
+	printf("%d", k);
+	return (0);
+}
