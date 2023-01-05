e3aadae00e6ab725d741e872711fe00952994c05
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-05 13:53:35 +0800

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -20,10 +20,10 @@ int _pow_recursion(int x, int y)
 	}
 	else if (y == 1)
 	{
-		return(x);
+		return (x);
 	}
 	else
 	{
-		return (x * _pow_recursion(x,y - 1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
