c277345c1054aa387503a463fa34e19c2986640a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-05 09:31:13 +0800

    Update 0-puts_recursion.c
    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,14 @@ void _puts_recursion(char *s)
 {
 	{
 		_putchar(*s);
-		if(*s != '\0')
+		if(*s == '\0')
 		{
-			_puts_recursion(s + 1);
+			return;
 		}
 		else
 		{
-			_putchar('\n');
+			_puts_recursion(s + 1);
 		}
 	}
+	_putchar('\n');
 }
