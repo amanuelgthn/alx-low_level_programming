d5be4f0e588dcadb8c8aac0aeaf98e4a38a1016b
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-05 12:26:41 +0800

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,16 +8,20 @@
  **/
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	int i;
 	
-	if (*s != '\0')
-	{
-		return (0);
+	if (*s == '\0')
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_strlen_recursion(s + 1);
+		}
+		if (*s != '\0')
+		{
+			i++;
+			return (i);
+		}
 	}
-	else
-	{
-		_strlen_recursion(s + 1);
-		i++;
-	}
-	return (i);
 }
