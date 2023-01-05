f4e5ca30c5faf5f0d230c6e2331f4edea84aceb6
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-05 10:48:39 +0800

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,16 +8,13 @@
 **/
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar(*s);
-	}
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
