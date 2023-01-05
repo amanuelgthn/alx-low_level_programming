5a3a0609f4192e650a233700b0618bb155b3047a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-05 10:34:30 +0800

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include"main.h
+/**
+*_print_rev_recursion- print string in reverse using recursion
+*@s: string to be printed in reverse order
+*Return: 0 sucess
+**/
+void _print_rev_recursion(char *s)
+{
+  if (*s == '\0')
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(*s);
+		}
+		if (*s != '\0')
+		{
+			_puts_recursion(s + 1);
+		}
+	}
+}
