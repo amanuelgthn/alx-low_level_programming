1b91b0da41607280220ed90be9a26ac17f3d204b
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-06 08:54:49 +0800

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,5 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
-int _sqrt_finder(int n, int i);
+int _sqrt_finder(int k, int i);
 int _sqrt_recursion(int n);
