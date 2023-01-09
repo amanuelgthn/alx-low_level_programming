3bcb0c3b95a1121943c8e9aa412d1b9a952f17c7
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-09 16:43:18 +0800

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+/**
+*main - print own program name
+*@argv: arguments
+*@argc: number of arguments
+*Return:0 success
+**/
+int main(int argc, char *argv[])
+{
+	while (argc--)
+	{
+		printf("%c", argc);
+		printf("\n");
+	}
+	return (0);
+}
