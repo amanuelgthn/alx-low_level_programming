ec86bf7695c6d392a788819f99b5cc011e61c57f
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-09 09:06:35 +0800

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+/**
+*main - print own program name 
+*@argv: arguments
+*Return:0 success
+**/
+int main (char *argv[])
+{
+	printf("%c",argv[0]);
+	printf("\n");
+	return (o);
+}
+	
+	
