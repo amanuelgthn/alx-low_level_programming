3a07e8c86f63e2c50586052830bd61560a21e9f8
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-10 12:30:07 +0800

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 /**
- * main- summations number
- * @argc:0
+ *main- function to sum all given arguments 
+ *@argc:number of arguments
+ *@argv:arguments
+ *Return: 0 in sucess 1 upon encoutering error
+ **/
+int main(int argc, char(*argv[]))
+{
+	int i,j,k;
+	
+	for (i = 0; i < argc; i++)
+		
