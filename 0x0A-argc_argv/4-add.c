cce9f9fcef4034713c5506122bd1005563427d1f
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-10 15:00:37 +0800

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char(*argv[]))
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if(atoi(argv[i]) > 0 || atoi(argv[i]) <= 0
+			if(atoi(argv[i]) > 0 || atoi(argv[i]) <= 0)
 			{
 				k = k + atoi(argv[i]);
 			}
