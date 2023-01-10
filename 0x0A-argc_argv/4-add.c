ed000df5e3160dd4b8250ce7e4755c0a071f398f
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-10 13:59:49 +0800

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,5 +10,26 @@ int main(int argc, char(*argv[]))
 {
 	int i,j,k;
 	
-	for (i = 0; i < argc; i++)
+	if ( argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc + 1; i++)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				k = k + atoi(argv[i]);
+			}
+		}
+		printf("%d\n", k);
+		return (0);
+	}
+}
 		
