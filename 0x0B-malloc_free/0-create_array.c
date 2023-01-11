978bb94e6ae5c17e9361390b183f04c346b7e736
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-11 09:47:03 +0800

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,4 +8,18 @@
  **/
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *str;
+	
+	str = malloc(sizeof(char) * size);
+	
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else 
+	{
+		for (i = 0; i < size; i++)
+		{
+			
 	
