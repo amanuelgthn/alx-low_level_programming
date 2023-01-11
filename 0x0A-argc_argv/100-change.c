96f1759a8626773955688a15009f011617fd904a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-11 23:43:27 +0800

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,44 +2,45 @@
 #include<stdlib.h>
 /**
 *_coin - return the number of coins for change
-*return: number of coins for change 
+*return: number of coins for change
 **/
 int _coin(int money, int coins)
 {
 	int a[5] = {25, 10, 5, 2};
-  if(money==3)
+	
+  if (money == 3)
   {
-    return(2);
+    return (2);
   }
-	if(money >= a[coins] && coins <=4)
+	if (money >= a[coins] && coins <= 4)
 	{
-		if(money % a[coins] == 0)
+		if (money % a[coins] == 0)
 		{
 			return (money / a[coins]);
 		}
 		else
 		{
-			return (money / a[coins] + _coin(money % a[coins],coins+1));
+			return (money / a[coins] + _coin(money % a[coins], coins+1));
 		}
 	}
-	else if(money < a[coins] && money != 1)
+	else if (money < a[coins] && money != 1)
 	{
-		return(_coin(money,coins+1));
+		return (_coin(money,coins + 1));
 	}
   else
   {
-    return(1);
+    return (1);
   }
 }		
 /**
- * main- print the minimum number of coins to make change for an amount of money
+ * main- print the minimum number of coins to make change 
  * @argc:number of arguments
  * @argv:arguments
  * Return: 1 if no arguments passed, 0 if argument passed is a negative number
  **/
 int main(int argc, char *argv[])
 {
-	int i,number;
+	int i, number;
 
 	if (argc == 1)
 	{
@@ -48,14 +49,14 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		number= atoi(argv[1]);
+		number = atoi(argv[1]);
 		if (number <= 0)
 		{
 			printf("0\n");
 		}
 		else
 		{
-			i=_coin(number,0);
+			i =_coin(number,0);
 			printf("%d\n",i);
 		}
 	}
