93b3fe0048b3cd88fcfff3b1268c94effbf30e53
amanuelgthn <amanuelgthn@gmail.com>
2023-01-12 07:35:01 -0800

    cocatenate

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"main.h"
+/**
+ * string_noncat- function that  concatenates two strings
+ * @s1:string 1
+ * @s2:string 2
+ * @n: length of string s2 to be concatenated to s1
+ * Return: pointer pointing to the concatenated strings 
+ **/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i,j;
+
+	i = strlen(s1);
+	j = 
+
