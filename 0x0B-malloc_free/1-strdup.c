142cb3df5e4485ad69b426a2bb5bebda7862fbc9
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-12 09:02:34 +0800

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,16 +12,16 @@ char *_strdup(char *str)
 	int i = strlen(str), j = 0;
 	
 	string = malloc(sizeof(char) * i + 1);
-	if (str == '\0')
-	{
-		return (NULL);
-	}
-	else
+	if (i >= 0)
 	{
 		for (j = 0; j < i; j++)
 		{
 			string[j] = str[j];
 		}
 		return (string);
+	}
+	else
+	{
+		return (NULL);
 	}
 }
