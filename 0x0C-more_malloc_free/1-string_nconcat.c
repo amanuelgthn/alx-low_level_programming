1a62e5e9f54697b1d9684eb3e6f88af399629f12
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-13 12:30:09 +0800

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,6 +18,25 @@ void *malloc_checked(unsigned int b)
 	}
 	return (a);
 }
+/**pass_s2 - pass the  string s2 to pointer
+*@str2: string to be passed
+*@ln: n bytes of strings to be passed
+*Return: pointer pointing to string s2
+**/
+char *pass_s2(char *str2, unsigned ln)
+{
+	char *strptr;
+	unsigned int l, m ;
+	
+	strptr = malloc_checked(sizeof(char) * (n));
+	m = strlen(str2);
+	for (l = 0; l < m && l < n; l++)
+	{
+		strptr[l] = str2[l];
+	}
+	rety
+	
+	
 /**
  * string_nconcat- function that  concatenates two strings
  * @s1:string 1
