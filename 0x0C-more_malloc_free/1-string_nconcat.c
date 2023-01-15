73af58155c1d46198ed5b436bfcf5d8b3780b764
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-15 23:33:46 +0800

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *pass_s2(char *str2, unsigned int ln)
 {
 	char *strptr;
 	unsigned int l, m;
-	
+
 	strptr = malloc_checked(sizeof(char) * (ln) + 1);
 	m = strlen(str2);
 	for (l = 0; l < m && l < ln; l++)
@@ -42,15 +42,15 @@ char *pass_s1(char *str1)
 	char *strptr1;
 	unsigned int l1, m1;
 
-	l1=strlen(str1);
+	l1 = strlen(str1);
 	strptr1 = malloc_checked(sizeof(char) * (l1) + 1);
-	for (m1 = 0; m1 < l1;m1++)
+	for (m1 = 0; m1 < l1; m1++)
 	{
 		strptr1[m1] = str1[m1];
 	}
 	free(strptr1);
 	return (strptr1);
-}		
+}	
 /**
  * string_nconcat- function that  concatenates two strings
  * @s1:string 1
@@ -64,35 +64,35 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, k, j = 0;
 
 	if (s1 == NULL && s2 == NULL)
-  {
-    str = malloc_checked(sizeof(char) * (n) + 1);
-    str[0] = '\0';
-    return (str);
-    }
-  else if (s1 == NULL || s1[0] == '\0')
+	{
+		str = malloc_checked(sizeof(char) * (n) + 1);
+		str[0] = '\0';
+		return (str);
+	}
+	else if (s1 == NULL || s1[0] == '\0')
 	{
 		str = pass_s2(s2, n);
 	}
 	else if (s2 == NULL || s2[0]=='\0')
-  {
-    str = pass_s1(s1);
-  }
-  else
+	{
+		str = pass_s1(s1);
+	}
+	else
 	{
 		i = strlen(s1);
 		str = malloc_checked(sizeof(char) * (i + n) + 1);
-	k = strlen(s1);
-	for (; j < i + n; j++)
-	{
-		if (j < i)
+		k = strlen(s1);
+		for (; j < i + n; j++)
 		{
-			str[j] = s1[j];
-		}
-		else if (j <= k + i)
-		{
-			str[j] = s2[j - i];
+			if (j < i)
+			{
+				str[j] = s1[j];
+			}
+			else if (j <= k + i)
+			{
+				str[j] = s2[j - i];
+			}
 		}
 	}
-    }
 	return (str);
 }
