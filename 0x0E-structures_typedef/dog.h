7c5b3abd2e407da3b9bb198831999b85eef79d92
amanuelgthn <amanuelgthn@gmail.com>
2023-01-16 03:59:42 -0800

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog-takes name age and owner of the dog
+ * @name:name of the dog
+ * @age:age of the dog
+ * @owner:owner of the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif /* _DOG_H_ */
