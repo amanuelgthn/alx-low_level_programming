fab867aa52ca336a8664dcc0f29413db0146a44a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-17 04:31:34 +0800

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,10 +3,8 @@
 #include<stdlib.h>
 #include"dog.h"
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	struct dog *d;
-	
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
+{	
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
