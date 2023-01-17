16cb329fddeb6064d13d0f5876a941f9ed1f6fd8
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-18 06:13:05 +0800

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,17 +21,6 @@ void print_dog(struct dog *d)
 		d->owner = "(nil)";
 	}
 	printf("Name: %s\n", d->name);
-<<<<<<< HEAD
-	if (d->age== float)
-	{
-		printf("Age: %f\n", d->age);
-	}
-	else
-	{
-		printf("Age:  0.000000\n");
-	}
-=======
 	printf("Age: %f\n", d->age);
->>>>>>> 5257d3543b16246db3b92c5576e924c975385623
 	printf("Owner: %s\n", d->owner);
 }
