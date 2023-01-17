4887b81de1cdad59343051001ebab615a0030e34
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-17 10:34:12 +0800

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,11 +23,11 @@ void print_dog(struct dog *d)
 	printf("Name: %s\n", d->name);
 	if (d->age)
 	{
-		printf("Age: (nil)\n");
+		printf("Age: %f\n", d->age);
 	}
 	else
 	{
-		printf("Age: %f\n", d->age);
+		printf("Age: (nil)\n");
 	}
 	printf("Owner: %s\n", d->owner);
 }
