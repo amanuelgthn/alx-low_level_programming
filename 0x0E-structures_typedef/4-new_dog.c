194d9be293f550d8f1de16330157a5b6ada82166
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-18 10:44:38 +0800

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,22 +21,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (newdog->name == NULL)
 	{
 		free (newdog->name);
+		free(newdog);
 		return (NULL);
 	}
 	else
 	{
 		strcpy(newdog->name,name);
 		free(newdog->name);
+		free(newdog);
 	}
 	if (newdog->owner == NULL)
 	{
 		free(newdog->owner);
+		free(newdog->name);
+		free(newdog);
 		return (NULL);
 	}
 	else
 	{
 		strcpy(newdog->owner,owner);
 		free(newdog->owner);
+		free(newdog->name);
+		free(newdog);
 	}
 	newdog->age = age;
 	free(newdog);
