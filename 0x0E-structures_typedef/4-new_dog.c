3a3c310e90665351fa7f13c85bd248f001687cd9
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-18 10:13:27 +0800

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *newdog;
 	
 	newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
+	{
+		return == NULL;
+	}
 	newdog->name = name;
 	newdog->age = age;
 	newdog->owner = owner;
