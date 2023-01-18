c2e37268d7c52cba36ee30e3153d9561f73d0b78
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-18 09:54:17 +0800

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
  **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t.name = name;
-	dog_t.age = age;
-	dog_t.owner = owner;
+	dog_t->name = name;
+	dog_t->age = age;
+	dog_t->owner = owner;
 }
