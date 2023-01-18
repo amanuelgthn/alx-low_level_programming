a9400388a93f67a7cdaf48f12162cb20c8d15482
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-18 10:58:58 +0800

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,37 +3,38 @@
 #include<string.h>
 #include"dog.h"
 /**
- * dog_t new_dog- new struct of dog_t type named new dog
+ * new_dog- new struct of dog_t type named new dog
  * @name:name of the new dog
  * @age: age of the new dog
  * @owner:owner of the new dog
+ * Return: new dog
  **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
 	int lenowner;
 	int lenname;
-	
-	lenowner=strlen(owner);
-	lenname=strlen(name);
+
+	lenowner = strlen(owner);
+	lenname = strlen(name);
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 	{
-		free (newdog);
+		free(newdog);
 		return (NULL);
 	}
-	newdog->name=malloc(lenname+1);
+	newdog->name = malloc(lenname + 1);
 	if (newdog->name == NULL)
 	{
-		free (newdog->name);
+		free(newdog->name);
 		free(newdog);
 		return (NULL);
 	}
 	else
 	{
-		strcpy(newdog->name,name);
+		strcpy(newdog->name, name);
 	}
-	newdog->owner=malloc(lenowner+1);
+	newdog->owner = malloc(lenowner + 1);
 	if (newdog->owner == NULL)
 	{
 		free(newdog->owner);
@@ -43,7 +44,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	else
 	{
-		strcpy(newdog->owner,owner);
+		strcpy(newdog->owner, owner);
 	}
 	newdog->age = age;
 	return (newdog);
