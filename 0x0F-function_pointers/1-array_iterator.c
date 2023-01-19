690416c6670db88e536a7f5743c31e1a8015ec25
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-19 08:57:54 +0800

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,11 +3,10 @@
 #include<string.h>
 #include"function_pointers.h"
 /**
-*f-function to print name
-*@d:name to be printed
-*print_name-function to print name
-*@name:name to be printed
-*@f:pointer to function
+*array_iterator-function to array
+*@array:array to be printed
+*@size:size of the array
+*@action:pointer to function
 **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
