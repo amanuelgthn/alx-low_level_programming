ddc22b658509d99eaf6499d233f311533921db20
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-19 09:30:34 +0800

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"function_pointers.h"
+int first_digit(n)
+{
+	int first;
+	
+	first = n;
+	while(first >= 10)
+	{
+		first = first / 10;
+	}
+	return (first);
+}
+/**
+*array_iterator-function to array
+*@array:array to be printed
+*@size:size of the array
+*@action:pointer to function
+**/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int long  i = 0;
+
+	if(size <= 0)
+	{
+		return (-1);
+	}
+	else if (array && size > 0 && cmp)
+	{
+		for (; i < size; i++)
+		{
+			if(cmp(action[i])) == 0));
+			{
+				return (-1);
+			}
+			else
+			{
+				first_digit(action[i]);
+			}
+				
+		}
+	}
+}
