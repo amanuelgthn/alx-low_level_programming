52c28134ae259f6e51fe3299f83345c35851d79a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-19 10:16:44 +0800

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,17 +2,6 @@
 #include<stdlib.h>
 #include<string.h>
 #include"function_pointers.h"
-int first_digit(int n)
-{
-	int first;
-	
-	first = n;
-	while(first >= 10)
-	{
-		first = first / 10;
-	}
-	return (first);
-}
 /**
 *array_iterator-function to array
 *@array:array to be printed
@@ -24,25 +13,27 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0,j;
 	
 	j = -1;
-	if(size <= 0)
+	if (array && cmp)
 	{
-		return (-1);
-	}
-	else
-	{
-		for (; i < size; i++)
+		if(size <= 0)
 		{
-			if(cmp(array[i]) > 0)
+			return (-1);
+		}
+		else
+		{
+			for (; i < size; i++)
 			{
-				j = i;
-				break;
-			}
-			if (cmp(array[i]) <=0 && i == size-1)
-			{
-				return(j);
+				if(cmp(array[i]) > 0)
+				{
+					j = i;
+					break;
+				}
+				if (cmp(array[i]) <=0 && i == size-1)
+				{
+					return (j);
+				}
 			}
 		}
-		
+	return (j);
 	}
-	return(j);
 }
