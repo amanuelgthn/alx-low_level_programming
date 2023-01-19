afcef47da6da3f4678fb4e7cfa237cb59e679864
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-19 09:32:42 +0800

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int first_digit(n);
 int int_index(int *array, int size, int (*cmp)(int));
 #endif /* _FUNCTIONS_POINTERS_ */
