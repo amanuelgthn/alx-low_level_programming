446f0f991051caf7ca61b5701d5247024e3223ec
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-19 09:00:14 +0800

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int long unsigned i;
-
-	if (size == 0)
-	{
-		return;
-	}
+	
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
