4d5687ab1442f31989898212847cedf50445c92c
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-19 10:46:03 +0800

    Create 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main- function to print the opcodes of a given machine)
+*@argc: number of arguments passed
+*@argv: arguments passed
+*Return:0
+**/
+int main(int argc, char *argv[])
+{
+	int i = 0, j;
+	if (argc < 2)
+	{
+		print("Error\n");
+		exit(1);
+	}
+	j = atoi(argv[1]);
+	if (j < 0)
+	{
+		print("Error\n");
+		exit(2);
+	}
+	for(; i < j; j++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < j -1)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	return (0);
+}		
