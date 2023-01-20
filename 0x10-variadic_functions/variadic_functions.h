236444c9bd038b7f7cb84a442c2aed0cfb9135a1
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-20 10:27:18 +0800

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,5 @@
 #ifndef _VARIADIC_FUNCTIONS_
 #define _VARIADIC_FUNCTIONS_
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif /* _VARIADIC_FUNCTIONS_ */
