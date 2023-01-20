93cb55f17838e77f5db5f8121d4ac8476af02e2a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-20 10:47:02 +0800

    Create 2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdarg.h>
+/**
+*print_strings - function to print all the arguments given followed by newline
+*@n:number of arguments
+*@separator:separator
+*Return: arguments or 0 if no arguments
+**/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *a;
+	va_list strings;
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+	
+	va_start(strings, n);
+
+	for (; i < n; i++)
+	{
+		a = va_arg(strings, char*);
+		if (a == NULL)
+		{
+			a = "(nil)";
+		}
+		printf("%s", s);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+
+	va_end(strings);
+}
