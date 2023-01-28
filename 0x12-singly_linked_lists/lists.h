152cc80df89628a2772d2d8e3c2bf4854847a1c4
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-28 15:54:34 +0800

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,18 @@
 #ifndef _LISTS_
 #define _LISTS_
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
 #endif /* _LISTS_ */
