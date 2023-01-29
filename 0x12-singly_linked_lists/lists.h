69bfacca719e99df288735d78eed5e29ab4ac90a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-29 10:37:00 +0800

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
 #ifndef _LISTS_
 #define _LISTS_
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
