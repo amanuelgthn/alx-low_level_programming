f7de9b7fc4b5c8e0e95829fd8b2acdd3fef7a272
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-29 16:01:58 +0800

    Create firstlinked.c

---
+++ 0x12-singly_linked_lists/firstlinked.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *link;
+}node;
+int main()
+{
+	node *head = NULL;
+	head = malloc(sizeof(node));
+	head->data = 45;
+	head->link = NULL;
+	printf("%d",head->data);
+	return 0;
+}
