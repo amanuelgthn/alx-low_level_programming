d44831ca3c403851fab4dd4c85bad4a88783251c
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-31 06:28:47 +0800

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,21 @@
 #include"lists.h"
 /**
 *print_list-print all the elements of a list and return number if nodes
-*@h-constant singly linked list
+*@h:constant singly linked list
+*Return:number of nodes
 **/
 size_t print_list(const list_t *h)
 {
 	const list_t *current_node = h;
 	int count = 0;
-	
-	if(current_node == NULL)
+
+	if (current_node == NULL)
 	{
 		printf("[0] (nil)\n");
 	}
-	while(current_node)
+	while (current_node)
 	{
-		if(current_node->str == NULL)
+		if (current_node->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
@@ -23,7 +24,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", current_node->len, current_node->str);
 		}
 		current_node = current_node->next;
-		count = count +1;
+		count = count + 1;
 	}
 	return (count);
 }
