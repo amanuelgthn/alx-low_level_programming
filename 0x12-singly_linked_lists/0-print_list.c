c9c4754a735068b207c6312bab3f6c5d9a499864
amanuelgthn <amanuelgthn@gmail.com>
2023-01-29 20:10:01 -0800

    linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,3 +4,14 @@
 *@h-constant singly linked list
 **/
 size_t print_list(const list_t *h)
+{
+	list_t *current_node = h;
+	int count = 0;
+
+	while(current_node != NULL)
+	{
+		printf("%d", current_node->str);
+		count = count +1;
+	}
+	return (count);
+}
