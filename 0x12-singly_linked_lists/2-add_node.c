3e3d7919c60442c8132c62257b734d1644d4b163
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-31 21:33:59 +0800

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,1 +1,23 @@
+#include"lists.h"
+/**
+*list_len-print number of elements in a single linked list
+*@h:constant singly linked list
+*Return:number of nodes
+**/
+size_t list_len(const list_t *h)
+{
+	const list_t *current_node = h;
+	int count = 0;
 
+	while (current_node)
+	{
+		if (current_node->str == NULL)
+		{
+			count = count;
+		}
+		current_node = current_node->next;
+		count = count + 1;
+	}
+	return (count);
+	;
+}
