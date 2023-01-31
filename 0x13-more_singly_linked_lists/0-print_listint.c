a317cffa05e5c8ad9ce23b2994f8739fe435789a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-01 06:29:40 +0800

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,26 @@
+#include"lists.h"
+/**
+*print_list-print all the elements of a list and return number if nodes
+*@h:constant singly linked list
+*Return:number of nodes
+**/
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current_node = h;
+	int count = 0;
 
+	while (current_node)
+	{
+		if (current_node->n == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%d\n", current_node->n);
+		}
+		current_node = current_node->next;
+		count = count + 1;
+	}
+	return (count);
+}
