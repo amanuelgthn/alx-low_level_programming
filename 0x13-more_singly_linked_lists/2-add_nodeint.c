4d527491f1146f36b70f6ee430a8ee6bd46fe4de
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-01 22:45:42 +0800

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint(listint_t **head, const int k)
 	}
 	new_node->n = k;
 	new_node->next = *head;
-	head = new_node;
+	*head = new_node;
 	free(new_node);
 	return (new_node);
 }
