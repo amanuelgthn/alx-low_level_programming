d0039221553e981aa58fb75d1b98ed78f99f1647
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-01 08:06:36 +0800

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,1 +1,18 @@
+#include"lists.h"
+/**
+*listint_len-print all the elements of a list and return number if nodes
+*@h:constant singly linked list
+*Return:number of nodes
+**/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	const listint_t *new_node = h;
+	new_node->;
 
+	while (current_node)
+	{
+		current_node = current_node->next;
+		count = count + 1;
+	}
+	return (count);
+}
