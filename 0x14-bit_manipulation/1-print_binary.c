0607da0dc5dacf051cd8977274a5f4321bfd12f9
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-04 08:54:23 +0800

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -16,9 +16,46 @@ unsigned long int pow(unsigned long int x,unsigned long int y)
 }
 unsigned long int findpower(unsigned long int n, unsigned long int k)
 {
+	unsigned long int i = 0;
 	
+	if (n < (pow(2, i)))
+	{
+		return (findpower(n, i + 1));
+	}
+	else
+	{
+		return (i - 1);
+	}
+}	
 /*
 *print_binary-covert integer to binary and print result
 *@n:integer
 **/
 void print_binary(unsigned long int n)
+{
+	unsigned long int i, j;
+	
+	if (n == 0)
+	{
+		printf("0\n");
+	}
+	else if (n == 1)
+	{
+		printf("1\n");
+	}
+	else
+	{
+		j = findpower(n);
+		for(i=j; i > 0; i--)
+		{
+			if(n >= pow(2,i))
+			{
+				printf("1");
+			}
+			else
+			{
+				printf("0");
+			}
+		}
+	}
+}
