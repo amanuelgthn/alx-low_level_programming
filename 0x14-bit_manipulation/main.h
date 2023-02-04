7d633ef16766f53e64b965758d8ba9b0b23f5842
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-04 09:03:59 +0800

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,7 @@
 unsigned int binary_to_uint(const char *b);
 int _pow_recursion(int x, int y);
 int _strlen(const char *s);
-unsigned long int pow(unsigned long int x,unsigned long int y);
+unsigned long int pow_recur(unsigned long int x,unsigned long int y);
 unsigned long int findpower(unsigned long int n, unsigned long int k);
 void print_binary(unsigned long int n);
 #endif /* _MAIN_ */
