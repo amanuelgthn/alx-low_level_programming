011f930ddcee40d908b2bf0d2ffbbae66e24d24a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-05 16:42:53 +0800

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,47 +1,20 @@
-#include"main.h"
-int pow_recur(int x,int y)
-{
-	if (y == 0)
-	{
-		return (1);
-	}
-	else  if (y == 1)
-	{
-		return (x);
-	}
-	else
-	{
-		return (x * pow_recur(x, y - 1));
-	}
-}
-int findpower(int n, int k)
-{
-	if (n >= (pow_recur(2, k)))
-	{
-		return (findpower(n, k + 1));
-	}
-	else
-	{
-		return (k - 1);
-	}
-}	
+#include"main.h"	
 /*
 *print_binary-covert integer to binary and print result
 *@n:integer
 **/
 void print_binary(unsigned long int n)
 {
-	int i = findpower(n,0),k;
-	for (; i >= 0; i--)
+	if (n == 0)
 	{
-		k = n >> i;
-		if (k & 1)
-		{
-			printf("1");
-		}
-		else
-		{
-			printf("0");
-		}
+		putchar('0');
+		return;
 	}
-}		
+	else if (n == 1)
+	{
+		putchar ('1');
+		return;
+	}
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
+}
