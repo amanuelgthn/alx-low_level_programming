09c83eb1b5511e82b83f3399ef98f12f50a6ad9b
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-06 08:01:16 +0800

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,3 +1,20 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	
+	{
+	int k,;
+
+	k = n >> index;
+	if (index > 63)
+	{
+		return(-1);
+	}
+	else if (k & 1)
+	{
+		k = 1;
+	}
+	else
+	{
+		k  0;
+	}
+	return (1);
+}
