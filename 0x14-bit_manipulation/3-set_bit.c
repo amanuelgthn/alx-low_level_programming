3a4cfec563e7331168faae288bce5bdc52eca601
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-06 08:35:03 +0800

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,3 +1,10 @@
+#include"main.h"
+/**
+*get_bit-function to get the bit of a binary at a given index
+*@n:integer
+*@index:index of the binary to be returned
+*Return:the bit of a binary at a given index or -1 if out of index
+**/
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	{
