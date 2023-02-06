d67ef3ae2f5ab92dc95939be764c4601b8ec24f3
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-06 16:23:28 +0800

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,8 @@
+#include"main.h"
+/**
+*read_textfile-read text file filename
+*@letters:number of letters in filename to read and printed
+*Return:0 if filename is empty and read fails.
+**/
+ssize_t read_textfile(const char *filename, size_t letters)
+  
