545d7aaf48b1654543356ec21b1945c40aed82da
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-17 12:04:04 +0800

    Update 1-readline.c

--- PID & PPID & simple shell Exercises/1-readline.c
+++ PID & PPID & simple shell Exercises/1-readline.c
@@ -12,13 +12,13 @@ int main()
 	char *buffer;
 	size_t bufsize = 32;
 	size_t characters;
-	buffer = malloc(bufsize * sizeof(char));
+	buffer =(char*)malloc(bufsize * sizeof(char));
 	if (buffer == NULL)
 	{
 		exit(1);
 	}
 	characters = getline(&buffer, &bufsize, stdin);
-	printf("\$ \n");
+	printf("$ \n");
 	printf("%zu\n", characters);
 	printf("%s\n", buffer);
 	return(0);
