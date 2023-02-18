0b043e5a962511f98ec45dc763e0634ba4edcab4
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-18 14:38:22 +0800

    Update 2-main.c

--- PID & PPID & simple shell Exercises/2-main.c
+++ PID & PPID & simple shell Exercises/2-main.c
@@ -4,9 +4,6 @@ int main()
 	char *str_token;
 	char str[80] = "This is - www.tutorialspoint.com - website";
 	str_token = split_str(str);
-	if (str_token!=NULL)
-	{
-		printf( " %s\n", str_token );
-	}
+	printf( " %s\n", str_token );
 	return(0);
 }
