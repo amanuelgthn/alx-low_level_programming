7cf25e0bd1fb8d8f8ff5c27b610613e02cafcd83
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-20 20:55:40 +0800

    Update 2-main.c

--- PID & PPID & simple shell Exercises/2-main.c
+++ PID & PPID & simple shell Exercises/2-main.c
@@ -1,15 +1,4 @@
 #include"main.h"
-int main()
-{
-	char *str_token;
-	char str[80] = "This is www tutorialspoint com website";
-	str_token = split_str(str);
-	while(str_token!=NULL)
-	{
-	    printf( " %s\n", str_token);
-	 }
-	return(0);
-}
 int main ()
 {
 	int nwords = 0;
