cdaba5163c9bc24cdfe231cc9bc431da824d1c75
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-21 14:05:23 +0800

    Update 2-main.c

--- PID & PPID & simple shell Exercises/2-main.c
+++ PID & PPID & simple shell Exercises/2-main.c
@@ -4,7 +4,7 @@ int main ()
 	int nwords = 0;
 	int i = 0;
 	char str[] = "This is www tutorialspoint com website";
-	char **arr = split_str(str, &nwords);
+	char **arr = split_str(str);
 	for (int i = 0; i < nwords; i++)
 	{
 		printf ("word %d: %s\n", i, arr[i]);
