f7e99383e1fff7fa385bbf0d2802ba1ed2dabb26
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-21 14:12:34 +0800

    Update 2-split_string.c

--- PID & PPID & simple shell Exercises/2-split_string.c
+++ PID & PPID & simple shell Exercises/2-split_string.c
@@ -1,50 +1,51 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 /**
- *split_str- splits the string and return the array of each word separated
- *@str:string to be separated
- *Return:return the array of each word separted
- **/
-char **split_str(char *str, int *words)
+ * str_split - Splits a string
+ * @str: The string that will be splited
+ * 
+ * Return: On success, it returns the new array
+ * of strings. On failure, it returns NULL
+ */
+char **str_split(char *str, int *number_of_words)
 {
-	int i = 0;
-	char *piece, **str_arr, *str_cpy = NULL;
-	
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	str_cpy = strdup(str);
-	piece = strtok(str_cpy, " ");
-	{
-		while(piece!=NULL)
-		{
-			if(*piece == '\n')
-			{
-				piece = strtok(NULL, " ");
-				continue;
-			}
-			(*words)++;
-			piece = strtok(NULL, " ");
-		}
-	}
-	str_arr= (char**)malloc(sizeof(char*)*(*words));
-	piece =strtok(str, " ");
-	for (i = 0; piece!=NULL; i++)
-	{
-		if (*piece == '\n')
-		{
-			piece = strtok(NULL, " ");
-			continue;
-		}
-		str_arr[i]=(char*)malloc(sizeof(char)*(strlen(piece)+1));
-		strcpy(str_arr[i],piece);
-		piece = strtok(NULL, " ");
-	}
-	if(str_cpy)
-	{
-		free(str_cpy);
-	}
-	return(str_arr);
+    char *piece, **str_arr = NULL, *str_cpy = NULL;
+    int i = 0;
+
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+    str_cpy = strdup (str);
+    piece = strtok(str_cpy, " ");
+    while (piece != NULL)
+    {
+        if ((*piece) == '\n')
+        {
+            piece = strtok(NULL, " ");
+            continue;
+        }
+        (*number_of_words)++;
+        piece = strtok(NULL, " ");
+    }
+
+    str_arr = (char **)malloc(sizeof(char *) * (*number_of_words));
+    piece = strtok(str, " ");
+    for (i = 0; piece != NULL; i++)
+    {
+        if ((*piece) == '\n')
+        {
+            piece = strtok(NULL, " ");
+            continue;
+        }
+        str_arr[i] = (char *)malloc(sizeof(char) * (strlen(piece) + 1));
+        strcpy(str_arr[i], piece);
+        piece = strtok(NULL, " ");
+    }
+
+    if (str_cpy)
+        free (str_cpy);
+
+    return (str_arr);
 }
