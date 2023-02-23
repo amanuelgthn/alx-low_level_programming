f4c16ebc10c87c07c4666f2db08b843606c9280e
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-23 12:29:22 +0800

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,7 @@ void more_more_numbers(void)
 
 	for (; i < 15; i++)
 	{
-		if (i < 10)
-		{
-			_putchar(i + '0');
-		}
-		else
-		{
-			j = i - 10;
-			_putchar(1 + '0');
-			_putchar(j + '0');
-		}
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
