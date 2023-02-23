13e2f26f042b4fb9f0fa0d865ed58907ae761676
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-23 10:05:18 +0800

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+*print_numbers-print numbers from 0 to 9 followed by new line
+**/
+void print_numbers(void)
+{
+	int i = 0;
+	
+	for(;i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
