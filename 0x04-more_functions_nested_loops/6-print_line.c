a0d533d47747c5ae0ebfa5659d732a95ced829a4
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-23 19:23:13 +0800

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,13 @@
 #include"main.h"
 /**
 *print_line-print line at a length of given number n
+*@n:length of line 
 **/
 void print_line(int n)
 {
 	int i = 0;
-	
-	for (;i < n; i++)
+
+	for (; i < n; i++)
 	{
 		_putchar('_');
 	}
