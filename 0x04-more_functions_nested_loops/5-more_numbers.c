e981dd5fcd4a3ecf0343c3f18314d756bf02b0f2
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-23 12:20:30 +0800

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+*more_more_numbers-print numbers from 0 to 14,10 times
+**/
+void more_more_numbers(void)
+{
+	int i = 0;
+	
+	for (; i < 15; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
+/**
+*more_numbers-print numbers from 0 to 14,10 times
+**/
+void more_numbers(void)
+{
+	int i = 0;
+	
+	for (; i < 11; i++)
+	{
+		more_more_numbers();
+	}
+}
