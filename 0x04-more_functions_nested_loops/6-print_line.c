fe6dda50afd20c4431c9f4dbcbab5de76795da2c
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-23 13:27:17 +0800

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+*print_line-print line at a length of given number n
+**/
+void print_line(int n)
+{
+	int i = 0;
+	
+	for (;i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
