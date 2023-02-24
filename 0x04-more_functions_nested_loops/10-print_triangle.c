8a89909e041eb89f2c196331e1e21539cc786e58
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-24 15:33:20 +0800

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,11 +13,11 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (; i <= size; i++)
+		for (; i < size; i++)
 		{
 			for (j = 0; j <= size; j++)
 			{
-				if (size - i >= j)
+				if (size - i > j)
 				{
 					_putchar(' ');
 				}
