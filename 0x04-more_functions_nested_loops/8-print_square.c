88830ecacf760f239dcaf8ab5edbdc9d4f61b561
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-24 12:23:41 +0800

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,13 +6,20 @@
 void print_square(int size)
 {
 	int i = 0, j;
-	
-	for (; i < size; i++)
+
+	if (size < 1)
 	{
-		for (j = 0; j < size; j++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
