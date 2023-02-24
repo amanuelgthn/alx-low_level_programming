78b32d8006f5b7314b7602b922283fdef81c2371
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-24 09:46:28 +0800

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+*print_diagonal-print the character / in diagonal
+*@n:length of the diagonal
+**/
+void print_diagonal(int n)
+{
+	int i = 0,j = 0;
+	
+	for (;i < n; i++)
+	{
+		if (i < n - 2)
+		{
+			_putchar(" ");
+		}
+		else
+		{
+			_putchar("\");
+		}
+	}
