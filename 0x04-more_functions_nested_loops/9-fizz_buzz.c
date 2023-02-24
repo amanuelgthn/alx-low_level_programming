bd797e9f058288c268d547447e1973e238633486
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-24 13:17:30 +0800

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,4 +9,23 @@ int main()
 
 	for (; i < 100; i++)
 	{
-		if (
+		if (i % 3 = 0 && i % 5 = 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 = 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 = 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d",i);
+		}
+		printf(" ");
+	}
+return (0);
+}
