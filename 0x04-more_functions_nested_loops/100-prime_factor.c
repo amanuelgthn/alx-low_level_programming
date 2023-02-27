9afbdebe9d7a63190449f93ab176f85acc7d9903
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-27 14:23:18 +0800

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,47 @@
 #include"main.h"
+#include <math.h>
 /**
 *main- function to finds and prints the largest prime factor of the number 612852475143
 *Return: 0 in sucess
 **/
-int main(void)
+long long maxPrimeFactors(long long n)
 {
-	int n = 612852475143;
-	int i = 0, j = 0;
+	long long maxPrime = -1;
 	
+	while (n % 2 == 0)
+	{
+		maxPrime = 2;
+		n >>= 1;
+	}
+	while (n % 3 == 0)
+	{
+		maxPrime = 3;
+		n = n / 3;
+	}
+	for (int i = 5; i <= sqrt(n); i += 6)
+	{
+		while (n % i == 0)
+		{
+			maxPrime = i;
+			n = n / i;
+		}
+		while (n % (i + 2) == 0)
+		{
+			maxPrime = i + 2;
+			n = n / (i + 2);
+		}
+	}
+	if (n > 4)
+	{
+		maxPrime = n;
+	}
+	return maxPrime;
+}
+int main()
+{
+
+    long long n = 612852475143;
+
+    printf("%lld\n", maxPrimeFactors(n));
+    return 0;
+}
