e32c922bb5a9362e70a1e5ac18fa0190eb2d3990
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-28 11:57:34 +0800

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,58 +1,31 @@
 #include<stdio.h>
-
 #include<string.h>
-
 /**
-
  *main - print first 50 fibonacci numbers
-
  *Return: 0 success
-
  **/
-
 int main(void)
-
 {
-
-	long int  a, i, j, k, m;
+	long int  a, i, j, k;
 
 	j = 1;
-
 	k = 2;
-	
-
 	printf("%ld", j);
-
 	printf(", ");
-
 	printf("%ld", k);
-
 	printf(", ");
 
-	for (i = 2 ; i <= 49 ; i++)
-
+	for (i = 2 ; i <= 98 ; i++)
 	{
-
 		a = k + j;
-
 		printf("%ld", a);
-
 		j = k;
-
 		k = a;
-
-		if (i != 49)
-
+		if (i != 98)
 		{
-
 			printf(", ");
-
 		}
-
 	}
-
 	printf("\n");
-
 	return (0);
-
 }
