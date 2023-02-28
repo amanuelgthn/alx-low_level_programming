322eb5593ab643fef71bdd5494400ee238d83aaa
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-28 10:43:49 +0800

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ *main - print first 50 fibonacci numbers
+ *Return: 0 success
+ **/
+int main(void)
+{
+	long int  a, i, j, k;
+
+	j = 1;
+	k = 2;
+	printf("%ld", j);
+	printf(", ");
+	printf("%ld", k);
+	printf(", ");
+
+	for (i = 2 ; i < 4000000 ; i++)
+	{
+		a = k + j;
+		if ( a % 2 == 0)
+		{
+		printf("%ld", a);
+		}
+		j = k;
+		k = a;
+		if (i != 49)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
