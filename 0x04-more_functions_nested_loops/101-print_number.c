ba7392a207af85932da2b8c970e7942b1275f2ec
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-28 10:10:12 +0800

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,16 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}
 	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 	}
 	if (n/10)
 	{
 		print_number(n/10);
 	}
-	putchar(n%10 + '0');
+	_putchar(n%10 + '0');
 }
