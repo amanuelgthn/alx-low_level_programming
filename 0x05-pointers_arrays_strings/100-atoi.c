e35685de75fb7d756c2aa4171d74e0daa855598e
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-02 20:18:02 +0800

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*_atoi- function to convert a string into an integer
+*@s:string to be converted into an integer
+*Return:the converted integer
+int _atoi(char *s)
+{
+	int i;
+	
+	i = s[0];
+	return(i);
+}	
