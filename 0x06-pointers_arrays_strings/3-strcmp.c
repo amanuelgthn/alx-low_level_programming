dbb29dfc712aa8b78ed1dbadaca308aa97ba8845
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-03 13:28:29 +0800

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,11 +2,34 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *_strncpy- Short description, single line
- *@dest:input nuber
- *@src:string to be copied
- *@n:length
- *Return: Always 0.
+ *_strncpy- function that compares two strings.
+ *@s1:string 1
+ *@s2:string 2
+ *Return: positive number if the first non-matching character is greater than s2
  **/
 int _strcmp(char *s1, char *s2)
-
+{
+	int i = 0, j,k,result;
+	
+	j = strlen(s1);
+	k = strlen(s2);
+	
+	for (;i < j || i < k; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			res = 0;
+			continue;
+		}
+		else if (s1[i] > s2[i])
+		{
+			res = 15;
+			break;
+		}
+		else if (s1[i] < s2[i])
+		{
+			res = -15;
+		}
+	}
+	return (res);
+}		
