f17f1a71ba7c2e1500af659bb8eb52aa61dd1766
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-03 14:13:26 +0800

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *_strncpy- function that compares two strings.
+ *_strcmp- function that compares two strings.
  *@s1:string 1
  *@s2:string 2
  *Return: positive number if the first non-matching character is greater than s2
@@ -10,10 +10,9 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0, j,k,res;
-	
+
 	j = strlen(s1);
 	k = strlen(s2);
-	
 	for (;i < j || i < k; i++)
 	{
 		if (s1[i] == s2[i])
@@ -29,7 +28,8 @@ int _strcmp(char *s1, char *s2)
 		else if (s1[i] < s2[i])
 		{
 			res = -13;
+			break;
 		}
 	}
 	return (res);
-}		
+}
