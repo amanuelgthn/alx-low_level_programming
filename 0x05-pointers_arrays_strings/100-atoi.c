1391606f81c3c344c793e766ae35790537445e6f
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-03 09:48:28 +0800

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,8 +10,20 @@ int _atoi(char *s)
 {
 	unsigned int i = 0;
 	unsigned int num = 0;
+	unsigned int sign = 1;
+	
 	for (; s[i] != '\0'; ++i)
 	{
+		if (s[i] == '-')
+		{
+			sign = sign * -1;
+			continue;
+		}
+		if (s[i] == '+');
+		{
+			sign = sign * 1;
+			continue;
+		}	
 		if (s[i] < 48 || s[i] > 57)
 		{
 			return (0);
@@ -21,5 +33,6 @@ int _atoi(char *s)
 			num = num * 10 + s[i] - 48;
 		}
 	}
+	num = sign * num;
 	return (num);
 }
