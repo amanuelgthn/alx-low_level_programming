f17efa2ee73e3d0ddc9290d51eb967c24e131432
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-05 15:48:30 +0800

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <limits.h>
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
