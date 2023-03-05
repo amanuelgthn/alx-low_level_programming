140850a78308d09c751e10854234712934cb1922
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-05 10:08:48 +0800

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,19 +1,18 @@
 #include"main.h"
 /**
-*cap_string-a function that encodes a string to 1337
+*leet-a function that encodes a string to 1337
 *@s:string to be encoded
 *Return:pointer to the encoded string
 **/
 char *leet(char *s)
 {
-	int i = 0,j, m;
-	
+	int i = 0, j, m;
 	char a[] = {'a', 'e', 'o', 't', 'l' };
 	int n[] = {52, 51, 48, 55, 49};
-	
+
 	j = strlen(s);
 	for (; i < j; i++)
-	{ 
+	{
 		for (m = 0; m < 5; m++)
 		{
 			if (s[i] == a[m] || s[i] == a[m] - 32)
