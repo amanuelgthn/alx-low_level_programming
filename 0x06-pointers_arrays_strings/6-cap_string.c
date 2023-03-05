1bda9523d2dcce81577d65efc2fc28db24c5cc2f
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-05 09:05:03 +0800

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,14 +6,25 @@
 **/
 char *cap_string(char *s)
 {
-	int i = 0, j, capitalize = 32;
+	int i = 0, j, m,k = 12, capitalize = 32;
+	int separator[] = {9, 10, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125}
 
 	j = strlen(s);
 	for (; i < j; i++)
 	{
-		if ( s[i] >= 97 && s[i] <= 122)
+		if (s[i] >= 97 && s[i] <= 122)
 		{
 			s[i] = s[i] - capitalize;
+		}
+		capitalize = 0;
+		for (m = 0; m <= k; k++)
+		{
+			if (s[i] == separator[m])
+			{
+				printf("%c",separator[m]);
+				capitalize = 32;
+				k = 12;
+			}
 		}
 	}
 	return (s);
