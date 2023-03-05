2ea91fb5ba141fdd4d01dddc0d6c221bbd98cadb
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-05 10:42:05 +0800

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,20 +6,18 @@
 **/
 char *rot13(char *s)
 {
-	int i = 0, j, m;
-	char a[] = {'a', 'e', 'o', 't', 'l' };
-	int n[] = {52, 51, 48, 55, 49};
+	int i = 0, j;
 
 	j = strlen(s);
 	for (; i < j; i++)
 	{
-		for (m = 0; m < 5; m++)
+		if ((s[i]>=65 && s[i]<=77) || (s[i]>=97 && s[i] <= 109))
 		{
-			if (s[i] == a[m] || s[i] == a[m] - 32)
-			{
-				s[i] = n[m];
-				break;
-			}
+			s[i] = s[i] + 13;
+		}
+		else
+		{
+			s[i] = s[i] - 13;
 		}
 	}
 	return (s);
