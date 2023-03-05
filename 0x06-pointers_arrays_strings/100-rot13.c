2985bf24f1c4d6afc160493cba7abe1ca8d57517
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-05 11:17:06 +0800

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,16 +1,16 @@
 #include"main.h"
 /**
 *rot13-function that encodes a string using the rot13
-*@s-string to be encoded
+*@s:string to be encoded
 *Return:the pointer to the encoded string
 **/
 char *rot13(char *s)
 {
-	int i = 0, j, m,k;
+	int i = 0, j, m, k;
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	k =sizeof(a)/sizeof(char);
+	k =sizeof(a) / sizeof(char);
 	j = strlen(s);
 	for (; i < j; i++)
 	{
