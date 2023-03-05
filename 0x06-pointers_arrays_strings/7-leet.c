9a3ecaab8b296d7e959f3a1683540e44c25b0c21
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-05 09:44:42 +0800

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,25 +11,9 @@ char *leet(char *s)
 	j = strlen(s);
 	for (; i < j; i++)
 	{ 
-		if ( s[i] >= 'a' || s[i] <= 'A')
+		if (s[i] >= 'a' || s[i] <= 'A')
 		{
 			s[i] = 52;
-		}
-		if ( s[i] >= 'e' || s[i] <= 'E')
-		{
-			s[i] = 51;
-		}
-		if ( s[i] >= 'o' || s[i] <= 'O')
-		{
-			s[i] = 48;
-		}
-		if ( s[i] >= 't' || s[i] <= 'T')
-		{
-			s[i] = 55;
-		}
-		if ( s[i] >= 'l' || s[i] <= 'L')
-		{
-			s[i] = 49;
 		}
 	}
 	return (s);
