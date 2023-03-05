0715bce38ba09f323a5816d793595dc1a59239bb
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-05 15:12:42 +0800

    Create  main_8.c

---
+++ 0x05-pointers_arrays_strings/main_8.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_number(INT_MIN);
+	_putchar('\n');
+	return (0);
+}
