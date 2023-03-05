c5e8de1b8717a3df5a4e89005883ae55c0c64268
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-05 11:04:52 +0800

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,10 +6,9 @@
 **/
 char *rot13(char *s)
 {
-	int i = 0, j, sign;
+	int i = 0, sign;
 
-	j = strlen(s);
-	while((s[i]>=65 && s[i]<= 90) || s[i]>=97 && s[i] <= 122) && s[i]!='\0')
+	while((s[i]>=65 && s[i]<= 90) || s[i]>=97 && s[i] <= 122))
 	{
 		if ((s[i]>=65 && s[i]<=77) || (s[i]>=97 && s[i] <= 109))
 		{
