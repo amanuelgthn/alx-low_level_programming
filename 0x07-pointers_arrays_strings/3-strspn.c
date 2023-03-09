66f67a9dd31edb5ddbe9c666d740ce72d93902b3
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-09 11:44:28 +0800

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+*_strspn-function that gets the length of a prrefix substring
+*@s- string
+*@accept-string
+*Return-number of bytes in the initial segment of s
+**/
+unsigned int _strspn(char *s, char *accept)
+{
+	
+	  
