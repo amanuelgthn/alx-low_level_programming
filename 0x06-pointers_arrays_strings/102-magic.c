470c129963354d25102986d206429df41fde435a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-09 09:32:55 +0800

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,23 @@
-#include"main.h"
-/**
-*infinite_add-
-*@n1:
-*@n2:
-*@r:
-*@size_r:
-*Return:
-**/
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+#include <stdio.h>
+
+int main(void)
 {
-	
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  ;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
