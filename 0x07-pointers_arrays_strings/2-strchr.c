762160f410e296e138c6f38898ece9ae62b3e32e
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-09 11:35:25 +0800

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,7 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0,str_len;
-	char *res;
+	char **res;
 	
 	str_len = strlen(s);
 	for (; i < str_len; i++)
@@ -16,7 +16,7 @@ char *_strchr(char *s, char c)
 		if (s[i] == c)
 		{
 			res[0]= c;
-			return (res);
+			return (*res);
 			break;
 		}
 		else
