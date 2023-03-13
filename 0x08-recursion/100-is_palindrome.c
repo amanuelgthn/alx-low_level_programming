19ef445b473d46fc10274bb9ac0cc830738da092
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-13 13:09:33 +0800

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,8 +5,13 @@ char *str_slice(char *src)
 {
 	int i = 1;
 	int len = strlen(src);
-	char *dest[len - 2];
+	char *dest;
 
+	dest = malloc(sizeof(char) * (len - 1));
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
 	for (; i < len - 1; i++)
 	{
 		dest[i - 1] = src[i];
@@ -22,13 +27,13 @@ int is_palindrome(char *s)
 {
 	int len = strlen(s);
 	
-	if (len % 2 == 0)
+	if (len == 0)
+	{
+		return (1);
+	}
+	else if (len % 2 == 0)
 	{
 		return (0);
-	}
-	else if (s == "")
-	{
-		returm (1);
 	}
 	else if (s[0] != s[len - 1])
 	{
