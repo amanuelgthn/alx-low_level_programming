37eded593ac572d3cab2a8082397c3f509efaa5f
amanuelgthn <amanuelgthn@gmail.com>
2023-03-17 03:00:37 -0700

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,18 +1,1 @@
-#include"lists.h"
-/**
-*sum_dlistint_t-returns the sum of all data in the doubly linked list
-*@head:doubly linked list
-*Return:the sum of all the data in the list
-**/
-int sum_dlistint(dlistint_t *head)
-{
-	int sum = 0;
-	dlistint_t *check = head;
 
-	while(check)
-	{
-		sum = sum + check->n;
-		check = check->next;
-	}
-	return (sum);
-}
