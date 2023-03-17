75e902c0e3e5c160df5a7f2ce831219ded40c594
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-17 17:09:11 +0800

    Update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -8,7 +8,7 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 	
-	while(head->next != NULL && head =!NULL)
+	while(head->next != NULL && head != NULL)
 	{
 		sum = sum + head->n;
 		head = head->next;
