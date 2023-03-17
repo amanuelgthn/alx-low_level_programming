6e7242c20e2bceae9cc4b5c976c00536adf89bad
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-17 10:27:01 +0800

    Create 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,8 @@
+#include"lists.h"
+/**
+*print_dlistint-print all elemensts in a doubly linked list
+*@h:a doubly linked list to be printed
+*Return: the number of nodes or elements in the doubly linked list 
+**/
+size_t print_dlistint(const dlistint_t *h)
+{
