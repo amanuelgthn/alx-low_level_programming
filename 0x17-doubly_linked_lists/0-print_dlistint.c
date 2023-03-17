cf580e7c892a2ab5e5892dc832c487536fb87f68
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-17 10:42:43 +0800

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,13 +2,13 @@
 /**
 *print_dlistint-print all elemensts in a doubly linked list
 *@h:a doubly linked list to be printed
-*Return: the number of nodes or elements in the doubly linked list 
+*Return: the number of nodes or elements in the doubly linked list
 **/
 size_t print_dlistint(const dlistint_t *h)
 {
 	int n = 0;
 	const dlistint_t *ptr = h;
-	
+
 	while (ptr != NULL)
 	{
 		printf("%d\n", ptr->n);
