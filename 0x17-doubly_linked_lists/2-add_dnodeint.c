0abe750538ccb6619584e5f9ca46a30ea1ab9404
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-17 11:23:39 +0800

    Create 2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include"lists.h"
+/**
+*dlistint_len-return the number of elemensts in a doubly linked list
+*@h:a doubly linked list to be printed
+*Return: the number of nodes or elements in the doubly linked list
+**/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+{
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	head->prev = new;
+	new->next = head;
+	new->prev = NULL;
+	head = new;
+	free(new);
+	return (new);
+}
