7e7b6fd1136f83a7109a27d2305b484b3339a0b7
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-17 16:46:04 +0800

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,6 @@
 #include"lists.h"
 /**
-*get_dnodeint_at_index-function that returns the nth node of a distint_t linked list
+*get_dnodeint_at_index-function that returns the nth node of a distint_t list
 *@head:a doubly linked list
 *@index:index of the node starting from 0
 *Return:the node of the linked list at the given index
