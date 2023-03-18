fb666dc4a6a279f62880bf6c979e5b672e3fb499
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-18 16:01:27 +0800

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,9 +8,10 @@
 **/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0;
+	unsigned int i = 0 count;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *tmp;
 
+	count = size_t dlistint_len(*h)
 	if (new_node == NULL)
 	{
 		return (NULL);
@@ -26,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-	else
+	else if (idx < count)
 	{
 		tmp = *h;
 		while (i < idx - 1 && tmp->next != NULL)
