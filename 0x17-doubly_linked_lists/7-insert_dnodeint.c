1108f5e161875660810a9e97072b82c28e7ed972
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-18 15:42:10 +0800

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->next = *h;
 			(*h)->prev = new_node;
 		}
-		h* = new_node;
+		*h = new_node;
 		return (new_node);
 	}
 	else
