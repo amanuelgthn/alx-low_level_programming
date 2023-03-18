71970a99323b2832839398277bfba5946a64a2c1
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-18 10:42:29 +0800

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,57 @@
 #include"lists.h"
 /**
-*
+*insert_dnodeint_at_index-function that inserts a new node at a given index of Linked list
+*@h:a doubly linked list
+*@idx:index of the list where the new node should be added starting from 0
+*@n:new node data
+*Return:the address of the new node or NULL if it failed
+**/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int n = 0, count = 0;
+	dlistint_t *check =malloc(sizeof(dlistint_t));
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	if (*h == NULL)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*h = new_node;
+		return (new_node);
+	}	
+	check = *h;
+	while(check->next != NULL)
+	{
+		check = check->next;
+		count = count + 1;
+	}
+	if (count == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = h;
+		h->prev = new_node;
+		return (new_node);
+	}
+	else
+	{
+		while (n < idx && n <= count)
+		{
+			h = h->next;
+		}
+		new_node->next = h->next;
+		h->next = new_node;
+		new_node->prev = h;
+		if (new_node->next != NULL)
+		{
+			new_node->next->prev = new_node;
+		}
+		return (new_node);
+	}
+	return (NULL);
+}
+		
