54d6c8a437dbcffe92aeade5f4a0a8146bc9b3f1
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-19 09:11:50 +0800

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -40,7 +40,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else if (index < count)
 	{
-		while (i < index - 1 && tmp->next != NULL)
+		while (i < index - 2 && tmp->next != NULL)
 		{
 			tmp = tmp->next;
 			i = i + 1;
