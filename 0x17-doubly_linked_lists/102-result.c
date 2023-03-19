99c5ae6153563ebae103484f1b532597aafbfa8b
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-19 15:07:32 +0800

    Update 102-result.c

--- 0x17-doubly_linked_lists/102-result.c
+++ 0x17-doubly_linked_lists/102-result.c
@@ -8,13 +8,13 @@ int isPalindrome(int n)
 {
 	int tmp = n, r = 0, sum = 0;
 	
-	while (n > 1)
+	while (n != 0)
 	{
 		r = n % 10;
 		sum = (sum * 10) + r;
 		n = n / 10;
 	}
-	if ( tmp == sum)
+	if ( tmp == n)
 		return (1);
 	return (-1);
 }
