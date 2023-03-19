85f4f5dbd7c53429b439ef7bd6d88dda62b0dad8
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-19 09:49:31 +0800

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -29,7 +29,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	
 	tmp = *head;
 	count = dlistint_len(*head);
-	if (index == 0 && (tmp))
+	if (index == 0)
 	{
 		if ((*head)->next != NULL)
 		{
