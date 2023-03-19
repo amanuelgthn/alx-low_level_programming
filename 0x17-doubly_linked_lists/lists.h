80abf06d82416a026de35d4e6f4cab19e8b73587
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-19 15:02:28 +0800

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -31,4 +31,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int isPalindrome(int n);
 #endif /* _LISTS_ */ 
