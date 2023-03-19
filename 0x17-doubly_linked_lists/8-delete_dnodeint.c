3db5b3c8fdf9df14e9d73a1cf802154475f15fb1
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-19 09:40:04 +0800

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -38,6 +38,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 				(*head) = (*head)->next;
 				(*head)->prev = NULL;
 			}
+			else
+				*head = NULL;
 		}
 		return (1);
 	}
