a6481fa0ed35bd365ca4d327b1ad5fd925c0a7a2
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-19 14:48:37 +0800

    Create 102-result.c

---
+++ 0x17-doubly_linked_lists/102-result.c
@@ -0,0 +1,8 @@
+#include"lists.h"
+/**
+*isPalindrome-function to find if a number is palindrome or not
+*@n-number
+*Return:1 if the number is palindrome or -1 if not
+**/
+int isPalindrome(int n)
+{
