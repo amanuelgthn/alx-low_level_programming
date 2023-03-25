49fc5b3ed024911bec5680d01633d03877692933
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-25 14:46:23 +0800

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,5 +2,5 @@
 #define __function_like_macro_h__
 #include <stdio.h>
 #include <stdlib.h>
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#define ABS(x) ((x < 0) ? ((x) * (-1)) : (x))
 #endif /* __function_like_macro_h__ */
