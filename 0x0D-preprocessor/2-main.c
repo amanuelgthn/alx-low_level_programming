8ab69958e918f0c7aa0a2504a15c2dcf54f024f0
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-25 14:49:47 +0800

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 /**
+*main-function to print the name of the file it was compiled from
+*Return: 0 on success
 **/
-int main(int argc,char argv[])
+int main()
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", __FILE__);
 	return (0);
 }
