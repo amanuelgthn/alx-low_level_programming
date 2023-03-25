95615ab29b0220bed844c75aa28a4c0a37527dd4
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-25 17:08:28 +0800

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,7 @@
 void Startup(void) __attribute__((constructor));
 /**
 *Startup-function executed before main function
+**/
 void Startup(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
