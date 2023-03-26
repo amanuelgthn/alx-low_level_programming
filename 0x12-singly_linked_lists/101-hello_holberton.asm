31461b20cfb4dac7f692af59c0db330fb1897eab
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-26 15:39:09 +0800

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,21 @@
-          global    _start
+        extern	printf		; the C function, to be called
 
-          section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
+        section .data		; Data section, initialized variables
+msg:	db "Hello world", 0	; C string needs 0
+fmt:    db "%s", 10, 0          ; The printf format, "\n",'0'
 
-          section   .data
-message:  db        "Hello, World", 10    
+        section .text           ; Code section.
+
+        global main		; the standard gcc entry point
+main:				; the program label for the entry point
+        push    rbp		; set up stack frame, must be alligned
+	
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; or can be  xor  rax,rax
+        call    printf		; Call C function
+
+	pop	rbp		; restore stack
+
+	mov	rax,0		; normal, no error, return value
+	ret			; return
