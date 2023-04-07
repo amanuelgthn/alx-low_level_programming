f23a3e6e67522c64bda6a27d50cb2a5d6c277486
amanuelgthn <amanuelgthn@gmail.com>
2023-04-07 01:26:30 -0700

    update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	unsigned long int i = 0;
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (table == NULL)
@@ -16,9 +15,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = (hash_node_t **)malloc(sizeof(hash_table_t) * size);
 	if (table->array == NULL)
 		return (NULL);
-	for (; i < table->size; i++)
-	{
-		table->array[i] = NULL;
-	}
 	return (table);
 }
