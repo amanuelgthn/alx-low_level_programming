489b08e8c1d5d257e04edcd29e681f42ddf60ffe
amanuelgthn <amanuelgthn@gmail.com>
2023-04-07 03:59:14 -0700

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,13 +6,13 @@
 **/
 unsigned long int hash_djb2(const unsigned char *str)
 {
-        unsigned long int hash;
-        int c;
-        
-        hash = 5381;
-        while((c = *str++))
-        {
-                hash = ((hash << 5) + hash) + c;
-        }
-        return (hash);
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while (c = *str++)
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
 }
