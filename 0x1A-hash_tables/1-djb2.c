17f5d0b938b27378065991c96f87d1ee598f470c
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-07 18:51:37 +0800

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -4,7 +4,7 @@
 *@str: string
 *Return: Hash value
 **/
-unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str)
 {
         unsigned long int hash;
         int c;
