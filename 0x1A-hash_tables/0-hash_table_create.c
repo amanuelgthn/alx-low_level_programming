62200e44528106cdd3881f3b74f4b146f99f04fe
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-07 16:55:14 +0800

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i = 0;
 
-	table = malloc(sizeof(hash_table_t));
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (table == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n")
 		return (NULL);
+	}
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n")
 		return (NULL);
+	}
 	for (; i < size; i++)
+	{
 		table->array[i] = NULL;
+	}
 	return (table);
 }
