65453ec17c097b8c2b239a4b3c4d9b876a1f7dad
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-08 10:58:24 +0800

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,34 +1,5 @@
 #include"hash_tables.h"
 /**
-*add_node-adds a new node at the beginning of a list_t list
-*@head: a singly linked list
-*@str:string to be added to the node
-*Return:the address of the new element or NULL if it failed
-**/
-/**list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new = malloc(sizeof(list_t));
-	int str_len = strlen(str);
-
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->str = strdup(str);
-	new->len = str_len;
-	if ((*head) == NULL)
-	{
-		new->next = NULL;
-		(*head = new);
-	}
-	else
-	{
-		new->next = (*head);
-		(*head) = new;
-	}
-	return (new);
-}**/
-/**
 *create_node- create hash node of the new element
 *@key:key
 *@value:value of key
@@ -55,9 +26,7 @@ hash_node_t* create_node(const char *key, const char *value)
 	strcpy(node->value, value);
 	node->next = NULL;
 	return node;
-}
-			   
-				   
+}				   
 /**
 *hash_table_set-function that adds an element to the hash table
 *@ht:hash table
@@ -76,6 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if(strcmp(current->key, key) == 0)
 		{
+			free(current->value);
 			current->value = strdup(value);
 			return (1);
 		}
