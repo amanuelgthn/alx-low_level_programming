b48bcf9b0b35145d74acb640a7dc735cee53ccdb
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-08 13:37:09 +0800

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,7 +12,9 @@ void hash_table_print(const hash_table_t *ht)
 		for (; i < ht->size; i++)
 		{
 			if(ht->array[i])
+				printf("{");
 				printf("'%s': '%s'",ht->array[i]->key,ht->array[i]->value);
 		}
+		printf("}\n");
 	}
 }
