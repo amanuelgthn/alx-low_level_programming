23fe8bca3f4a4444f8a994fad97d7705252025f0
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-08 10:27:13 +0800

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -31,6 +31,7 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
+hash_node_t* create_node(const char *key, const char *value);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
