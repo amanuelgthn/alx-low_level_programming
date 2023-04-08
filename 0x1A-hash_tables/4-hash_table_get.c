01e1a2b6223cd787a10f47e724c9ec5acf363f77
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-08 12:17:23 +0800

    Create 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,15 @@
+#include"hash_tables.h"
+/**
+*hash_table_get-function that retrieves a value associated with a key
+*@ht:hash table
+*@key:key
+*Return:the value associated with the element or NULL if key couldn't be found
+**/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int long ind;
+	
+	ind = key_index((unsigned char *)key, ht->size);
+	if (table->array[i] == NULL)
+		return NULL;
+}
