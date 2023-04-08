7f7f9b7edf3df4a33037a7da6218775c2d7cfaf6
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-08 12:46:59 +0800

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	else
 	{
 		current = ht->array[ind];
-		while(current->next)
+		while(current->next && current != NULL)
 		{
 			if (strcmp(current->key, key) == 0)
 				return (current->value);
