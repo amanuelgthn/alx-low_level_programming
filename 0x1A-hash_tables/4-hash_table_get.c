6de2982d997fb60c1663e35893f242f9797f8438
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-08 12:56:43 +0800

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,19 +13,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL)
 		return (NULL);
 	ind = key_index((unsigned char *)key, ht->size);
-	if (ht->array[ind] == NULL)
+	current = ht->array[ind];
+	if (current == NULL)
 		return (NULL);
-	else if(strcmp((*ht->array)->key, key) == 0)
-		return (ht->array[ind]->value);
-	else if (ht->array[ind])
+	else if(strcmp((current->key, key) == 0)
+		return (current->value);
+	/**else if (ht->array[ind])
 	{
-		current = ht->array[ind];
 		while(current->next && current != NULL && current->key && current->value)
 		{
 			if (strcmp(current->key, key) == 0)
 				return (current->value);
 			current = current->next;
 		}
-	}
+	}**/
 	return (NULL);
 }
