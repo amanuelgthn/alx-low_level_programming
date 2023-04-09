f3c9563e2ecd0d35aefb62fe93e26307f8759c6a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-09 09:42:09 +0800

    Update 5-main.c

--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -14,13 +14,13 @@ int main(void)
 
     ht = hash_table_create(1024);
     hash_table_print(ht);
-    /**hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "c", "fun");
     hash_table_set(ht, "python", "awesome");
     hash_table_set(ht, "Bob", "and Kris love asm");
     hash_table_set(ht, "N", "queens");
     hash_table_set(ht, "Asterix", "Obelix");
     hash_table_set(ht, "Betty", "Cool");
-    hash_table_set(ht, "98", "Battery Street");**/
+    hash_table_set(ht, "98", "Battery Street");
     hash_table_set(ht, "hetairas", "Street");
     hash_table_set(ht, "mentioner", "Battery");
     hash_table_print(ht);
