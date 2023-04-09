74a9d20d2135a1478617957eff13fcdfd8f32497
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-09 09:58:36 +0800

    Create 6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,43 @@
+#include"hash_tables.h"
+/**
+*free_list-free a singly linked list
+*@head:doubly linked list
+**/
+void free_list(hash_node_t *head)
+{
+
+	while (head)
+	{
+		free(head->key);
+		head->key = NULL;
+		free(head->value);
+		head->value = NULL;
+		free(head->next);
+		head->next = NULL;
+		free(head);
+		head = head->next;
+	}
+}
+/**
+*hash_table_delete-a function that delets a hash table
+*@ht:a hash table
+**/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0, j = 0;
+	hash_node_t *collision;
+
+	if (ht)
+	{
+		for (; i < ht->size; i++)
+		{
+			collision = ht->array[i];
+			while (collision)
+			{
+				free_list(collision)
+				collision = collision->next;
+			}
+		}
+		free(ht->size);
+	}
+}
