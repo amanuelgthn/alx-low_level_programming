d081a866bfaf5ba5b6e75b343e24a06be4847d47
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-09 10:00:52 +0800

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -24,7 +24,7 @@ void free_list(hash_node_t *head)
 **/
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0, j = 0;
+	unsigned long int i = 0;
 	hash_node_t *collision;
 
 	if (ht)
@@ -34,7 +34,7 @@ void hash_table_delete(hash_table_t *ht)
 			collision = ht->array[i];
 			while (collision)
 			{
-				free_list(collision)
+				free_list(collision);
 				collision = collision->next;
 			}
 		}
