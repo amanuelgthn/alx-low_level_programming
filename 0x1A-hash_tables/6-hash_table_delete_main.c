1f7be9333e9341ba1db0215df112a22d12ca8709
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-09 14:09:24 +0800

    Create 6-hash_table_delete_main.c

---
+++ 0x1A-hash_tables/6-hash_table_delete_main.c
@@ -0,0 +1,32 @@
+#include"hash_tables.h"
+/**
+*hash_table_delete-a function that delets a hash table
+*@ht:a hash table
+**/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0, j = 0;
+	hash_node_t *collision = NULL;
+
+	if (ht)
+	{
+		for (; i < ht->size; i++)
+		{
+			collision = ht->array[i];
+			while (collision)
+			{
+				free(collision->key);
+				collision->key = NULL;
+				free(collision->value);
+				collision->value = NULL;
+				collision = collision->next;
+			}
+			free_list(collision);
+			collision = NULL;
+		}
+		free(ht->array);
+		ht->array = NULL;
+		free(ht);
+		ht = NULL;
+	}
+}
