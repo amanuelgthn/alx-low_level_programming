813ff391cd1479c3e551c24550cab92fc5d0ec0f
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-06 14:13:02 +0800

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,14 +5,14 @@
 **/
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0;
+	unsigned long int i;
 	hash_node_t *next = NULL, *current = NULL;
 
 	if (ht == NULL)
 		return;
 	else
 	{
-		for (; i < ht->size; i++)
+		for (i = 0; i < ht->size; i++)
 		{
 			current = ht->array[i];
 			while (current != NULL)
@@ -23,11 +23,8 @@ void hash_table_delete(hash_table_t *ht)
 				free(current);
 				current = next;
 			}
-			next = NULL;
 		}
 		free(ht->array);
-		ht->array = NULL;
 		free(ht);
-		ht = NULL;
 	}
 }
