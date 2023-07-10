7fefe387945c65dcf18242fb48e45c969d3d0b1c
Amanuel_Bikora <53990706+amanuelgthn@users.noreply.github.com>
2023-07-10 20:44:52 +0800

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+void print_searching(int *array, size_t first, size_t last);
 
 #endif /* __search_algos__ */
