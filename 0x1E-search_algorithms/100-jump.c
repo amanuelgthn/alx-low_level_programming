e3c5fbe3efe77fdea83dcc057063fe4f0fa69125
Amanuel_Bikora <53990706+amanuelgthn@users.noreply.github.com>
2023-07-12 14:49:16 +0800

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -8,18 +8,26 @@
 **/
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i = 0, root, high = size - 1;
+	size_t i = 0, root, high = value - 1, found;
 
+	if (array == NULL)
+		return (-1);
 	root = sqrt(high);
 	for (; i < size; i++)
 	{
 		printf("Value checked array[%d] = [%d}\n", i, i);
 		if (array[i] == value)
 			return (i);
-		else if (array[root] >= value)
+		else if (array[i + root] >= value)
 		{
-			return (root);
-		else if (array[root] < value)
-		{
-			i = root
-
+			if (found == 0)
+			{
+				printf("Value found between indexes [%d] and [%d]\n", i, i + root);
+				found = 1;
+			}
+		}
+		else if (array[i + root] < value)
+			i += root;
+	}
+	return (-1);
+}
