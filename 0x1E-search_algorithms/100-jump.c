438d30174631172c4587888df5ccd9b75c121b02
Amanuel_Bikora <53990706+amanuelgthn@users.noreply.github.com>
2023-07-13 09:06:54 +0800

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,10 +1,11 @@
 #include"search_algos.h"
 /**
-* jump_search - function that searches for a value in a sorted array of integers
+* jump_search -function that searches for a value in a sorted array of integers
 * using the jump search algorithm
 * @array: pointer to the first element of the array to be searched
 * @size: number of elements in the array
-* Return: first index where the value is located or -1 if not present or the array is NULL
+* Return: first index where the value is located or -1 if not present or
+* the array is NULL
 **/
 int jump_search(int *array, size_t size, int value)
 {
