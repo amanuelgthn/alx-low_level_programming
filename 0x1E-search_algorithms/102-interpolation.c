5bb38d4e6d98ca685052de10fe6f873ea9c69ca3
Amanuel_Bikora <53990706+amanuelgthn@users.noreply.github.com>
2023-07-13 10:57:58 +0800

    Update 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,6 +12,8 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0, pos, high = size - 1;
 
+	if (array == NULL)
+		return (-1);
 	while ((array[high] != array[low]) && (value >= array[low])
 		&& (value <= array[high]))
 	{
