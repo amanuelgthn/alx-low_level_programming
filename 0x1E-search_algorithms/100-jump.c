ac4643cac341bd5130f6234dbd6e6f9e3ca65df2
Amanuel_Bikora <53990706+amanuelgthn@users.noreply.github.com>
2023-07-13 09:07:16 +0800

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,7 +17,7 @@ int jump_search(int *array, size_t size, int value)
 	for (; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%lu]\n", i, i);
-		if (array[i + root] >= value || i == size -1)
+		if (array[i + root] >= value || i == size - 1)
 		{
 			if (found == 0)
 			{
