9236ce2f891fcbea9992237e6d41f5b25c2c44cf
amanuelgthn <amanuelgthn@gmail.com>
2023-07-15 19:57:43 -0700

    Create 104-advanced_search.c

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,11 @@
+#include"search_algos.h"
+/**
+ * advanced_binary - function that searches for a value in a sorted array of intergers
+ * using advanced binary search
+ * @array - pointer to the first element of the array to be searched in
+ * @size - number of elements in array
+ * @value - value to be searched
+ * Return: index where value is located or -1 if array is NULL
+ * or value is not present
+ **/
+int advanced_binary(int *array, size_t size, int value)
