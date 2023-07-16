fc78544f016fe9d7d3557b7f0f6c09d968409469
amanuelgthn <amanuelgthn@gmail.com>
2023-07-16 11:00:08 +0800

    update

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,5 +1,25 @@
 #include"search_algos.h"
 /**
+ * print_searching - function to print the array in which
+ * search is being perfomed
+ * @array: pointer to the first element of the array to be printed
+ * @first: first index of the array to be printed
+ * @last: last index of the array to be printed
+ **/
+void print_searching(int *array, size_t first, size_t last)
+{
+	size_t i = first;
+
+	printf("Searching in array: ");
+	for (; i <= last; i++)
+	{
+		printf("%d", array[i]);
+		if (i < last)
+			printf(", ");
+	}
+	printf("\n");
+}
+/**
  * advanced_binary - function that searches for a value in a sorted array of intergers
  * using advanced binary search
  * @array - pointer to the first element of the array to be searched in
@@ -9,3 +29,22 @@
  * or value is not present
  **/
 int advanced_binary(int *array, size_t size, int value)
+{
+	size_t low = 0, mid, high = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	while (low <= high)
+	{
+		print_searching(array, low, high);
+		mid = (low + high) / 2;
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (mid);
+	}
+	return (-1);
+}
